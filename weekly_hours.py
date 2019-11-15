414a773c0c5c33b2a3855a6a03211affcbce5aab
Karl Mathias Moberg <karlmathias@gmail.com>
2019-11-15 23:30:44 +0100

    Release for weekly stats

--- weekly_hours.py
+++ weekly_hours.py
@@ -153,7 +153,8 @@ for i in range(week_ago.year, today.year + 1):
             prev_minutes = round(row[4] / 60 / 60, 1)
         current_hours = round(current_hours / 60 / 60, 1)
 
-        webhook_url = 'https://hooks.slack.com/services/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' #os.getenv('SLACK_WEBHOOK_GENERAL')
+        # os.getenv('SLACK_WEBHOOK_GENERAL')
+        webhook_url = 'https://hooks.slack.com/services/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'
 
         if current_hours > prev_minutes:
             message_data = {
