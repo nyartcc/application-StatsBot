4521014bb08b31569233f583ac7e54fefac1155a
Karl Mathias Moberg <karlmathias@gmail.com>
2019-11-15 19:48:41 +0100

    Bugfix

--- hour_statistics.py
+++ hour_statistics.py
@@ -41,7 +41,7 @@ for i in range(2016, 2020):
         run = mycursor.execute(my_query)
         hours = convertTuple(mycursor.fetchone())
 
-        if (run == None):
+        if (hours > 0):
             f.write("{}-{},{}\n".format(i, j, hours))
             print("{}-{}: {} -- {}-{}".format(i, j,
                                               round(hours / 60 / 60, 1), start_time, end_time))
