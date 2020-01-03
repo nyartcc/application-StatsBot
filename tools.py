c96834c6f7c866bde94d2a7869ddd7e981b32b1a
Karl Mathias Moberg <karlmathias@gmail.com>
2020-01-03 15:32:54 +0100

    Fixed weekly hour statistics and moved post to Discord to tools

--- tools.py
+++ tools.py
@@ -1,7 +1,39 @@
 import functools
 import operator
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import os
+from dotenv import load_dotenv
+
+discord_webhook = os.getenv('DISCORD_WEBHOOK_GENERAL')
 
 
 def convertTuple(tup):
     str = functools.reduce(operator.add, (tup))
     return str
+
+
+def post_to_discord(last_week_hours, this_week_hours, tc_fname, tc_lname, tc_cid, tc_hours):
+    # create embed object for webhook
+    embed = DiscordEmbed(title='Weekly Statistics',
+                         description='Statustics for the week of {}.{}.{}'.format(today.year, today.month, today.day), color=242424)
+
+    # set image
+    embed.set_thumbnail(
+        url='https://image.prntscr.com/image/mTFpZeXOR8_lGUTO8gVg-Q.png')
+
+    current_hours = round(current_hours / 60 / 60, 1)
+
+    embed.add_embed_field(name='This Weeks Hours',
+                          value='{}'.format(current_hours))
+    embed.add_embed_field(name='Last Weeks Hours',
+                          value='{}'.format(prev_minutes))
+    embed2 = DiscordEmbed(title='This weeks top controller:',
+                          description='{0} {1} - CID: {2} with {3} hours! Congratulations!'.format(tc_fname, tc_lname, tc_cid, tc_hours), color=242424)
+
+    # Post fun stuff to Discord
+    webhook = DiscordWebhook(url=discord_webhook)
+
+    # add embed object to webhook
+    webhook.add_embed(embed)
+    webhook.add_embed(embed2)
+    webhook.execute()
--- weekly_hours.py
+++ weekly_hours.py
@@ -7,7 +7,7 @@ import operator
 import functools
 import os
 import mysql.connector
-from tools import convertTuple
+from tools import convertTuple, post_to_discord
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 # Load the database variables from the .env file.
@@ -299,27 +299,5 @@ for i in range(week_ago.year, today.year + 1):
                     % (reponse.status_code, response.text)
                 )
 
-    # Post fun stuff to Discord
-    webhook = DiscordWebhook(url=discord_webhook)
-
-    # create embed object for webhook
-    embed = DiscordEmbed(title='Weekly Statistics',
-                         description='Statustics for the week of {}.{}.{}'.format(today.year, today.month, today.day), color=242424)
-
-    # set image
-    embed.set_thumbnail(
-        url='https://image.prntscr.com/image/mTFpZeXOR8_lGUTO8gVg-Q.png')
-
-    current_hours = round(current_hours / 60 / 60, 1)
-
-    embed.add_embed_field(name='This Weeks Hours',
-                          value='{}'.format(current_hours))
-    embed.add_embed_field(name='Last Weeks Hours',
-                          value='{}'.format(prev_minutes))
-    embed2 = DiscordEmbed(title='This weeks top controller:',
-                          description='{0} {1} - CID: {2} with {3} hours! Congratulations!'.format(tc_fname, tc_lname, tc_cid, tc_hours), color=242424)
-
-    # add embed object to webhook
-    webhook.add_embed(embed)
-    webhook.add_embed(embed2)
-    webhook.execute()
+            post_to_discord(last_week_hours, this_week_hours,
+                            tc_fname, tc_lname, tc_cid, tc_hours)
